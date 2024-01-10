a17ec44a903b0485e6be1f133c356e523987a164
Nay <56006347+nayeyar@users.noreply.github.com>
2024-01-10 11:21:15 -0600

    write valid test cases to pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 /** @jest-environment jsdom */
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, getAllByRole } from '@testing-library/react';
 import { App } from './App';
 
 /**
@@ -15,7 +15,13 @@ test('App should render', () => {
 
 test('Button should render', () => {
   // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  // expect('no test written').toBe('tested');
+  render(<App />);
+  const btns = screen.getAllByRole('button');
+  btns.forEach(element => {
+    expect(element).toBeInTheDocument();
+    expect(element).toBeEnabled();
+  });
 });
 
 /**
@@ -24,7 +30,16 @@ test('Button should render', () => {
  */
 test('theme button should update button text', () => {
   // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+  const btns = screen.getAllByRole('button');
+
+  expect(btns[0]).toHaveTextContent('Current theme: light');
+  fireEvent.click(btns[0]);
+  expect(btns[0]).toHaveTextContent('Current theme: dark');
+
+  expect(btns[1]).toHaveTextContent('Show hidden content');
+  fireEvent.click(btns[1]);
+  expect(btns[1]).toHaveTextContent('Hide hidden content');
 });
 
 // BONUS
@@ -32,7 +47,13 @@ test('theme button should update button text', () => {
 // e.g.: expect(element).toHaveStyle('color: #FFF');
 test('theme button should toggle styles', () => {
   // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  const bElm = render(<App />);
+  const btns = screen.getAllByRole('button');
+  expect(bElm.baseElement).toHaveStyle('color: #333');
+  expect(bElm.baseElement).toHaveStyle('backgroundColor: #FFF');
+  fireEvent.click(btns[0]);
+  expect(bElm.baseElement).toHaveStyle('color: #FFF');
+  expect(bElm.baseElement).toHaveStyle('backgroundColor: #333');
 });
 
 /**
@@ -46,7 +67,15 @@ test('theme button should toggle styles', () => {
  */
 test('hidden button should toggle hidden content', () => {
   // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+  const btns = screen.getAllByRole('button');
+  // const hiddenText = screen.queryByText('this content is hidden by default');
+  // expect(hiddenText).toBe(null);
+  // expect(screen.getByText('this content is hidden by default')).toThrow();
+  expect(screen.queryByText('this content is hidden by default')).not.toBeInTheDocument();
+  fireEvent.click(btns[1]);
+  expect(screen.getByText('this content is hidden by default')).toBeInTheDocument();
+
 });
 
 
